93fb9a1fee3f872d6d1d733178b480bd826aed86
Jacob <jacob.walker@accenture.com>
2021-07-28 10:59:49 +0100

    "Section 8"

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { IProduct } from './models/product';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  products:IProduct[];
+
+
+  constructor(private http:HttpClient) {
+    
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response:IPagination)=> {
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    });
+
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
   title = 'client';
 }
